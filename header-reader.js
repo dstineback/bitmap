514e330d6da139e8a1d8c652a8801ec2ce7b0756
Dan Stineback <dstineback@gmail.com>
2016-05-16 11:49:16 -0700

    last save before submit

--- header-reader.js
+++ header-reader.js
@@ -1,15 +1,12 @@
-'use strict'
-const fs = require('fs');
+'use strict';
+var fs = require('fs');
 
-const file = fs.readFileSync(__dirname + '/img/palette-bitmap.bmp');
+var file = fs.readFileSync(__dirname + '/img/palette-bitmap.bmp');
 
-const headers = {};
+var headers = {};
 console.log(file.slice(0, 4));
-
 console.log(file[0], file[1]);
 
 headers.type = file.toString('ascii', 0, 2);
 headers.size = file.readUInt32LE(2);
 headers.pixelStart = file.readUInt32LE(10);
-
-console.log(headers);
--- main.js
+++ main.js
@@ -3,5 +3,4 @@
 var transform = require(__dirname + '/lib/transformer');
 var send = require(__dirname + '/lib/send');
 
-
 transform(__dirname + '/img/palette-bitmap.bmp');
