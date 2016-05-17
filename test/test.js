c9ffa18555064513289c56da27c25038bca40f66
Dan Stineback <dstineback@gmail.com>
2016-05-16 19:51:40 -0700

    updated the buffer transform

--- test/test.js
+++ test/test.js
@@ -3,18 +3,9 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 var randomize = require('../lib/randomize');
-var stringify = require('../lib/stringify');
 var transformer = require('../lib/transformer');
 
-describe('stringify.js', function() {
-  var testArray = [[1],[2],[3],[4],[5]];
-	var stringifiedArray = stringify(testArray);
 
-	it('Should take an array turn them into strings', function(){
-		expect(typeof stringifiedArray).to.eql('string');
-		expect(typeof stringifiedArray[3]).to.eql('string');
-	});
-});
 
 describe('transform img', function() {
   it('should have 2 diffent imgs', function(){
